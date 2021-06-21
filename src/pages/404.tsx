0dae9a6d62c7c47550a145d75eb4a66d40aa6b1c
mahendrajena <53595991+mahendrajena@users.noreply.github.com>
2021-06-21 07:13:38 +0000

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,17 @@
-import NotFound from 'src/NotFound';
+import Head from 'next/head';
 
-const Custom404 = (): JSX.Element => <NotFound />;
+const Custom404 = (): JSX.Element => (
+  <>
+    <Head>
+      <title>404: Custom404</title>
+    </Head>
+    <div style={{ padding: 10 }}>
+      <h1>Page not found</h1>
+      <p>This page does not exist.</p>
+      <p>Page Generated @</p>
+      <a href="/">Go to the Home page</a>
+    </div>
+  </>
+);
 
 export default Custom404;
